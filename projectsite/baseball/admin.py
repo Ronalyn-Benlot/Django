d7a9f6f0171ba496e3e263994b958d2857448cca
Ronalyn <202080147@psu.palawan.edu.ph>
2022-11-14 11:22:15 +0800

    Changes

---
+++ projectsite/baseball/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.admin import display
+from .models import Position
+
+
+
+# Register your models here.
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    list_display=("description",)
+    search_fields=("description",)
