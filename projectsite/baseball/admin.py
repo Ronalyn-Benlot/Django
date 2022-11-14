2bf0c8a3f3c874a0f7f5d418cd7728cbf7a3890d
Ronalyn <202080147@psu.palawan.edu.ph>
2022-11-14 12:05:42 +0800

    Update

--- projectsite/baseball/admin.py
+++ projectsite/baseball/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import display
-from .models import Position
+from .models import Position, Person , Club , Play
 
 
 
@@ -10,3 +10,17 @@ from .models import Position
 class PositionAdmin(admin.ModelAdmin):
     list_display=("description",)
     search_fields=("description",)
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("lastname","firstname","height","weight")
+    search_fields = ("lastname",)
+
+@admin.register(Club)
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ("name","coach",)
+    search_fields = ("name",)
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("player","team","string_no","isActive",)
